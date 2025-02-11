f85aa92311e3fe7051117e4602355877234ceae4
Livia <freitasl@grinnell.edu>
2025-02-10 21:03:35 -0600

    Adding files to the repository

---
+++ project1.cpp
@@ -0,0 +1,62 @@
+/*
+ * CSC-301
+ * project1.cpp
+ * Spring 2025
+ *
+ * Project 1 - Sorting
+ *
+ * Partner 1: Princess Alexander
+ * Partner 2: Livia Stein Freitas
+ * Date: February 10th, 2025
+ */
+
+#include "project1.hpp"
+
+using namespace std;
+
+/*
+ * selectionSort
+ */
+void selectionSort(vector<double> &arrayToSort) {
+    return;
+}
+
+/*
+ * insertionSort
+ */
+void insertionSort(vector<double> &arrayToSort) {
+    return;
+}
+
+/*
+ * bubbleSort
+ */
+void bubbleSort(vector<double> &arrayToSort) {
+    return;
+}
+
+/*
+ * mergeSort
+ */
+void mergeSort(vector<double> &arrayToSort) {
+    return;
+}
+
+/*
+ * quickSortHelper
+ * 
+ * Note that i is inclusive (i.e., is the first element of subarray).
+ * Note that j is not inclusive (i.e., off the end of the subarray).
+ * 
+ */
+void quickSortHelper(vector<double> &arrayToSort, int i, int j) {
+    return;
+}
+
+/*
+ * quickSort
+ */
+void quickSort(vector<double> &arrayToSort) {
+    quickSortHelper(arrayToSort, 0, arrayToSort.size());
+    return;
+}
